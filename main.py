0a63c30360161ecd03545cf26e525a3344a9ef55
DaewonSeo <saz0549@naver.com>
2023-02-13 14:44:26 +0900

    crawler 동작 코드 추가

---
+++ main.py
@@ -0,0 +1,205 @@
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import time
+import gspread
+import telegram
+import os
+import logging
+import logging.handlers
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+SCOPE = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
+    ]
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132\
+    Safari/537.36'
+
+JSON_FILE = './google.json'
+
+def change_date_format(date):
+    """
+    날짜 포맷을 년.월.일. 형식으로 변경하는 함수
+    네이버 뉴스의 경우
+    0분전, 0시간 전, 0일전 등 7일 내 뉴스는
+    년.월.일이 아닌 다른 포맷으로 표시되므로 날짜를 통일해주는 함수가 필요함
+    """
+    current_time = datetime.now()
+    date = date.replace(" ", "")
+    if date.endswith('분전'):
+        minutes = int(date[:-2])
+        date = current_time - timedelta(minutes=minutes)
+
+    elif date.endswith('시간전'):
+        hours = int(date[:-3])
+        date = current_time - timedelta(hours=hours)
+
+    elif date.endswith('일전'):
+        days = int(date[:-2])
+        date = current_time - timedelta(days=days)
+
+    else:
+        date = datetime.strptime(date, '%Y.%m.%d.')
+    return date.strftime("%Y-%m-%d")
+
+
+def send_telegram_message(article):
+    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+    message = f"""[{article['날짜']}]\n{article['발행사']}의 기사 1건이 보도되었습니다.\n기사제목 : {article['제목']}\n{article['링크']}"""
+    bot.sendMessage(chat_id=os.environ["CHAT_ID"], text=message)
+
+
+def connect_file(SHEET_FILE_URL):
+    """구글 스프레드 시트에 접속 연결"""   
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_FILE, SCOPE)
+    gc = gspread.authorize(credentials)
+    doc = gc.open_by_url(SHEET_FILE_URL)
+    worksheet = doc.worksheet('시트1')
+    return worksheet
+
+def connect_news_db():
+    return connect_file(os.environ["NEWS_DB"])
+
+
+def next_available_row(worksheet):
+    """제일 최근 기사를 가져오기 위해 제일 하단 row로 접근"""
+    str_list = list(filter(None, worksheet.col_values(1)))
+    return str(len(str_list))
+
+
+def write_file(worksheet, data):
+    worksheet.append_row(data)
+
+
+def write_news_db(worksheet, article):
+    write_file(worksheet, [
+        article['제목'],
+        article['날짜'],
+        article['발행사'],
+        article['링크'],
+        article['요약'],
+        ])
+
+def write_log_db(worksheet, timestamp, message):
+    write_file(worksheet, [timestamp, message])
+
+
+def get_article(keyword, start_idx, latest_news, date_from, date_to):
+    """네이버 기사 가져오기"""
+    query = f'\"{keyword}\"'
+    search_date_from = date_from.replace("-", ".")
+    search_date_to = date_to.replace("-", ".")
+    params = {
+        'where': 'news',
+        'query': query,
+        'sm': 'tab_opt',
+        'sort': 1, # 0: 관련도순 1: 최신순 2: 오래된순
+        'photo': 0,
+        'field': 0,
+        'pd': 3, # 기간 설정과 관련된 키워드, 직접 입력시 3 / 전체 0 / 1시간 7 / 1주일 1 등 
+        'ds': search_date_from, # 시작일
+        'de': search_date_to, # 종료일
+        'docid': '',
+        'related': 0,
+        'mynews': 0,
+        'nso': 'so:dd,p:all,a:all',
+        'office_type': 0,
+        'office_section_code': 0,
+        'news_office_checked': '',
+        'is_sug_officeid': 0,
+        'start': start_idx,
+    }
+    
+    # html 요청
+    base_url = 'https://search.naver.com/search.naver?'
+    req = requests.get(base_url, params=params, headers={'User-Agent': USER_AGENT})
+
+    if req.status_code != 200:
+        print('error raised!')
+        return [], False
+    
+    # 파싱을 위한 뷰티풀 수프 객체 선언
+    soup = BeautifulSoup(req.text, 'html.parser')
+    news_list = soup.select('div.group_news div.news_area')
+    
+    results = []
+    date_from = datetime.strptime(date_from, '%Y-%m-%d')
+    date_to = datetime.strptime(date_to, '%Y-%m-%d')
+    # 페이지당 기사 순회
+    for news in news_list:
+        date = change_date_format(news.select('span.info')[-1].text)
+        current_date = datetime.strptime(date, '%Y-%m-%d')
+        if current_date >= date_from and current_date <= date_to:
+            publishing_company = news.select('a.info')
+            is_naver = publishing_company[1]['href'] \
+                if len(publishing_company) > 1 else '없음'
+            title = news.select_one('a.news_tit')['title']
+            url = news.select_one('a.news_tit')['href']
+            description = news.select_one('a.api_txt_lines.dsc_txt_wrap').text
+            article = {
+                '제목': title,
+                '날짜': date,
+                '발행사': publishing_company[0].text,
+                '네이버 발행': is_naver,
+                '링크': url,
+                '요약': description
+            }
+            if url == latest_news: # google sheet의 최신 기사와 바교하여, 같은 경우 프로그램 종료.
+                break
+
+            results.append(article)
+        else:
+            logger.info('검색기간에 맞지 않는 기사이므로 저장하지 않습니다.')
+            break
+        
+    return results
+
+
+def main():
+    keyword = '제11전투비행단'
+    load_news_db = connect_news_db()
+    last_row = next_available_row(load_news_db)
+    try:
+        latest_news = load_news_db.row_values(last_row)[3] # google sheet 시트 내 기사 링크 주소
+    except gspread.exceptions.APIError:
+        latest_news = None
+    
+    idx = 1
+    today = datetime.now().strftime('%Y-%m-%d')
+    while True:
+        articles = get_article(keyword, idx, latest_news, date_from='2023-01-01', date_to=today)
+        
+        if not articles:
+            logger.info('더이상 검색된 뉴스결과가 없으므로, 프로그램을 종료합니다')
+            load_news_db.sort((2, 'asc'))
+            break
+        else:
+            article_cnt = len(articles)
+            for _ in range(article_cnt):
+                article = articles.pop()
+                write_news_db(load_news_db, article)
+                send_telegram_message(article)
+            logger.info(f'총 {article_cnt}건의 보도자료가 db에 기록되었습니다')
+            idx += 10
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
